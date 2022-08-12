237b65ac3d10207fa5683a47177bcfb63fb2ff18
NicoA8 <nicoastancai@gmail.com>
2022-08-12 19:33:19 +0300

    Rezolvare string-templates

--- dist/index.8350ebce.js
+++ dist/index.8350ebce.js
@@ -548,14 +548,15 @@ const book2 = {
     author: "Jaroslav Hašek",
     localPrice: 50
 };
+function priceRonEuro(price) {
+    return `are prețul de ${price} RON (${price / 5} €).`;
+}
 function displayBook(book) {
-// Completează această funcție
+    return `${book.title} de ${book.author} ${priceRonEuro(book.localPrice)}`;
 }
 console.log(displayBook(book1));
-// Ar trebui să scrie: 
 // "Kéraban Încăpățânatul de Jules Verne are prețul de 30 RON (6 €)"
-console.log(displayBook(book2)); // Ar trebui să scrie: 
- // "Peripețiile bravului soldat Švejk de Jaroslav Hašek are prețul de 50 RON (10 €)"
+console.log(displayBook(book2)); // "Peripețiile bravului soldat Švejk de Jaroslav Hašek are prețul de 50 RON (10 €)"
 
 },{}]},["3ZNtQ","bhPPS"], "bhPPS", "parcelRequire94c2")
 
--- string-templates.js
+++ string-templates.js
@@ -1,30 +1,29 @@
 // Se dau următoarele variabile
 const currency = {
-    local: "RON",
-    international: "€",
-    exchangeRate: 5,
+  local: "RON",
+  international: "€",
+  exchangeRate: 5,
 };
+
 const book1 = {
-    title: "Kéraban Încăpățânatul",
-    author: "Jules Verne",
-    localPrice: 30
+  title: "Kéraban Încăpățânatul",
+  author: "Jules Verne",
+  localPrice: 30,
 };
 const book2 = {
-    title: "Peripețiile bravului soldat Švejk",
-    author: "Jaroslav Hašek",
-    localPrice: 50
+  title: "Peripețiile bravului soldat Švejk",
+  author: "Jaroslav Hašek",
+  localPrice: 50,
 };
-
-
+function priceRonEuro(price) {
+  return `are prețul de ${price} RON (${price / 5} €).`;
+}
 function displayBook(book) {
-    // Completează această funcție
+  return `${book.title} de ${book.author} ${priceRonEuro(book.localPrice)}`;
 }
 
 console.log(displayBook(book1));
-// Ar trebui să scrie: 
 // "Kéraban Încăpățânatul de Jules Verne are prețul de 30 RON (6 €)"
 
-
 console.log(displayBook(book2));
-// Ar trebui să scrie: 
 // "Peripețiile bravului soldat Švejk de Jaroslav Hašek are prețul de 50 RON (10 €)"
