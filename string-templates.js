52fc34a8fd44853c58f73fef0f4b798957169bda
Belei Bianca <biebianca99@gmail.com>
2022-08-12 20:36:37 +0300

    string-templates.js

--- string-templates.js
+++ string-templates.js
@@ -1,30 +1,31 @@
 // Se dau următoarele variabile
 const currency = {
-    local: "RON",
-    international: "€",
-    exchangeRate: 5,
+  local: "RON",
+  international: "€",
+  exchangeRate: 5,
 };
 const book1 = {
-    title: "Kéraban Încăpățânatul",
-    author: "Jules Verne",
-    localPrice: 30
+  title: "Kéraban Încăpățânatul",
+  author: "Jules Verne",
+  localPrice: 30,
 };
 const book2 = {
-    title: "Peripețiile bravului soldat Švejk",
-    author: "Jaroslav Hašek",
-    localPrice: 50
+  title: "Peripețiile bravului soldat Švejk",
+  author: "Jaroslav Hašek",
+  localPrice: 50,
 };
 
-
 function displayBook(book) {
-    // Completează această funcție
+  // Completează această funcție
+  return `${book.title} de ${book.author} are pretul de ${
+    book.localPrice
+  } RON (${book.localPrice / 5} €)`;
 }
 
 console.log(displayBook(book1));
-// Ar trebui să scrie: 
+// Ar trebui să scrie:
 // "Kéraban Încăpățânatul de Jules Verne are prețul de 30 RON (6 €)"
 
-
 console.log(displayBook(book2));
-// Ar trebui să scrie: 
+// Ar trebui să scrie:
 // "Peripețiile bravului soldat Švejk de Jaroslav Hašek are prețul de 50 RON (10 €)"
