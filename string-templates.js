a256559dc08dc4edb51ee6ed7c42dd469b1b63a1
AliceIoana <aliceioana1007@gmail.com>
2022-08-12 10:36:37 +0300

    solution

--- string-templates.js
+++ string-templates.js
@@ -1,30 +1,34 @@
 // Se dau următoarele variabile
 const currency = {
-    local: "RON",
-    international: "€",
-    exchangeRate: 5,
+  local: "RON",
+  international: "€",
+  exchangeRate: 5,
 };
 const book1 = {
-    title: "Kéraban Încăpățânatul",
-    author: "Jules Verne",
-    localPrice: 30
+  title: "Kéraban Încăpățânatul",
+  author: "Jules Verne",
+  localPrice: 30,
+  euroPrice: function (price) {
+    return this.localPrice * exchangeRate;
+  },
 };
 const book2 = {
-    title: "Peripețiile bravului soldat Švejk",
-    author: "Jaroslav Hašek",
-    localPrice: 50
+  title: "Peripețiile bravului soldat Švejk",
+  author: "Jaroslav Hašek",
+  localPrice: 50,
+  euroPrice: function (price) {
+    return this.localPrice * exchangeRate;
+  },
 };
 
-
 function displayBook(book) {
-    // Completează această funcție
+  return `${title} de ${author} are pretul de ${localPrice} ${local} (${euroPrice()} ${international})`;
 }
 
 console.log(displayBook(book1));
-// Ar trebui să scrie: 
+// Ar trebui să scrie:
 // "Kéraban Încăpățânatul de Jules Verne are prețul de 30 RON (6 €)"
 
-
 console.log(displayBook(book2));
-// Ar trebui să scrie: 
+// Ar trebui să scrie:
 // "Peripețiile bravului soldat Švejk de Jaroslav Hašek are prețul de 50 RON (10 €)"
