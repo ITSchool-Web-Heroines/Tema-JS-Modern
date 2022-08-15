198c03536765814981a0d131e6f9795e4ea76eca
anamariap21 <anapacurar321@gmail.com>
2022-08-15 21:02:47 +0300

    Afisare carti

--- string-templates.js
+++ string-templates.js
@@ -17,14 +17,17 @@ const book2 = {
 
 
 function displayBook(book) {
-    // Completează această funcție
+   return  {`${book.title} de ${book.author} are pretul de ${book.localPrice} ()`};
 }
 
 console.log(displayBook(book1));
+    
+
 // Ar trebui să scrie: 
 // "Kéraban Încăpățânatul de Jules Verne are prețul de 30 RON (6 €)"
 
 
 console.log(displayBook(book2));
+
 // Ar trebui să scrie: 
 // "Peripețiile bravului soldat Švejk de Jaroslav Hašek are prețul de 50 RON (10 €)"
